1c92837dbf7c535b5f6b6beec5c1854c35870ada
Zjerilon <zjerilon@Zjerilons-MacBook-Pro.local>
2024-02-28 10:55:00 +0100

    Scraped basic information from gamma

--- src/products/gamma.service.ts
+++ src/products/gamma.service.ts
@@ -7,20 +7,30 @@ export class GammaService {
     const browser = await puppeteer.launch({ headless: false });
     const [page] = await browser.pages();
     await page.goto('https://www.gamma.nl/assortiment/zoeken?text=schroeven');
-    // await page.waitForNavigation();
     const productsElements = await page.evaluate(() => {
       const products = document.querySelectorAll('.product-row-row-tile');
       return Array.from(products).map((product) => {
-        const [_, productImage, productContent] = product.children;
+        const [, productImage, productContent] = product.children;
+
         const titleElement = productContent.querySelector(
           '.product-tile-name > a',
         );
-        console.log(titleElement?.textContent);
-        const price = product.childNodes;
-        return { price };
+        const imageSrc = productImage
+          .querySelector('img')
+          ?.getAttribute('data-src');
+
+        const priceElements = productContent.querySelector(
+          '.product-tile-price',
+        )?.children;
+
+        const [priceCurrent, priceDecimal] = priceElements || [];
+        const price =
+          (priceCurrent.textContent ?? '') + (priceDecimal.textContent ?? '');
+
+        return { title: titleElement?.textContent, src: imageSrc, price };
       });
     });
-    // console.log(productsElements);
+
     return productsElements;
   }
 }
